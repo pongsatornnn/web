d031a7e5d97c6ad0fe2766b2d6cb0639e98681ee
pongsatornnn <pongsatorn25472547@gmail.com>
2024-09-09 01:32:21 +0700

    update

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import *
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('01', bamee),
+    path('02', baknut),
+    path('03', hoykom),
+    path('04', creater),
 ]
--- myapp/myapp/views.py
+++ myapp/myapp/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
  
 def index(request):
     return render(request, 'index.html')
+
+def bamee(request):
+    return render(request, 'bamee.html')
+
+def baknut(request):
+    return render(request, 'buknut.html')
+
+def hoykom(request):
+    return render(request, 'hoykom.html')
+
+def creater(request):
+    return render(request, 'creater.html')
